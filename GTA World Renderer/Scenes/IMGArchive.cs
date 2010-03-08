57d431304b0103d11fe09b19ea985d09825a9711
Dennis Yolkin <dennisyolkin@gmail.com>
2010-03-09 01:08:48 +0300

    IMGArchive: stub created

---
+++ GTA World Renderer/Scenes/IMGArchive.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GTAWorldRenderer.Scenes
+{
+   /// <summary>
+   /// Реализация загрузки и работы с IMG архивами.
+   /// в IMG архивах хранятся текстуры.
+   /// 
+   /// Описание формата IMG есть здесь: http://gtamodding.ru/wiki/IMG_%D0%B0%D1%80%D1%85%D0%B8%D0%B2
+   /// </summary>
+   class IMGArchive
+   {
+   }
+}
