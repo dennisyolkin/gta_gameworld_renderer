d4d1bcce4b9f7ca161699add8f0abf54bdfb4003
Dennis Yolkin <dennisyolkin@gmail.com>
2010-03-27 01:03:22 +0300

    Implemented turning on-off usage of mouse for camera rotation

--- GTA World Renderer/Rendering/SceneRenderer3D.cs
+++ GTA World Renderer/Rendering/SceneRenderer3D.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace GTAWorldRenderer.Rendering
 {
@@ -17,9 +18,13 @@ namespace GTAWorldRenderer.Rendering
       GraphicsDevice device;
       Camera camera;
       InfoPanelFor3Dview textInfoPanel;
-      MouseState originalMouseState;
       Effect effect;
       Matrix projectionMatrix;
+
+      KeyboardState oldKeyboardState = Keyboard.GetState();
+      MouseState originalMouseState;
+      bool usingMouse = true;
+
 
       /*
        * TODO :: 
@@ -70,6 +75,9 @@ namespace GTAWorldRenderer.Rendering
 
       private void ProcessMouse(GameTime gameTime, float amount)
       {
+         if (!usingMouse)
+            return;
+
          MouseState currentMouseState = Mouse.GetState();
          if (currentMouseState.LeftButton == ButtonState.Pressed && currentMouseState != originalMouseState)
          {
@@ -87,20 +95,37 @@ namespace GTAWorldRenderer.Rendering
       {
          Vector3 moveVector = new Vector3(0, 0, 0);
          KeyboardState keyState = Keyboard.GetState();
-         if (keyState.IsKeyDown(Keys.Up))
+
+         Func<Keys, bool> KeyDown = key => keyState.IsKeyDown(key);
+         Func<Keys, bool> KeyPressed = key => keyState.IsKeyDown(key) && !oldKeyboardState.IsKeyDown(key);
+
+         // Перемещение камеры
+         if (KeyDown(Keys.Up))
             moveVector += Vector3.Forward;
-         if (keyState.IsKeyDown(Keys.Down))
+         if (KeyDown(Keys.Down))
             moveVector += Vector3.Backward;
-         if (keyState.IsKeyDown(Keys.Right))
+         if (KeyDown(Keys.Right))
             moveVector += Vector3.Right;
-         if (keyState.IsKeyDown(Keys.Left))
+         if (KeyDown(Keys.Left))
             moveVector += Vector3.Left;
-         if (keyState.IsKeyDown(Keys.PageUp))
+         if (KeyDown(Keys.PageUp))
             moveVector += Vector3.Up;
-         if (keyState.IsKeyDown(Keys.PageDown))
+         if (KeyDown(Keys.PageDown))
             moveVector += Vector3.Down;
 
+         // Ускоренное перемещениеи камеры
          bool fast = keyState.IsKeyDown(Keys.LeftShift) || keyState.IsKeyDown(Keys.RightShift);
+
+         if (KeyPressed(Keys.Escape))
+         {
+            GTAWorldRenderer.Logging.Log.Instance.Print("Escape pressed!");
+            usingMouse = !usingMouse;
+            if (usingMouse)
+               Mouse.SetPosition(device.Viewport.Width / 2, device.Viewport.Height / 2);
+         }
+
+         oldKeyboardState = keyState;
+
          camera.UpdatePosition(moveVector * amount * (fast? fastMoveSpeed : slowMoveSpeed));
       }
 
