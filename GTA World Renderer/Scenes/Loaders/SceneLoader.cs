bd40b87bcee2fbeb3a58f7246ef182cee55dc9e2
Dennis Yolkin <dennisyolkin@gmail.com>
2010-04-22 13:01:17 +0400

    Clearing TexturesStorage cache when scene is loaded

--- GTA World Renderer/Scenes/Loaders/SceneLoader.cs
+++ GTA World Renderer/Scenes/Loaders/SceneLoader.cs
@@ -17,7 +17,7 @@ namespace GTAWorldRenderer.Scenes.Loaders
          {
             var sceneObjectsLoader = new SceneObjectsLoader();
             var sceneObjects = sceneObjectsLoader.LoadScene();
-            // TODO :: reset TextureStorage
+            TexturesStorage.Instance.Reset();
             var scene = CreateScene(sceneObjects);
             GC.Collect();
             return scene;
